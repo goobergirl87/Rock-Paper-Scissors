a1e0c2a1cbef17706c6c68cf530184743d223489
goobergirl87 <isabelheidekorn@gmail.com>
2023-02-24 13:28:12 +0100

    Set up function to get computer's choice

---
+++ rps.js
@@ -0,0 +1,14 @@
+// ROCK PAPER SCISSORS 
+
+function getComputerChoice() {
+    let random_pick = Math.floor(Math.random() * 3);
+    if (random_pick == 0) {
+      return 'rock';
+    }
+    else if (random_pick == 1) {
+      return 'paper';
+    }
+    else {
+      return 'scissors';
+    }
+  }
